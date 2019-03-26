ad51cf7809e5af94d4964074e558908272bbc920
Boney Bun <boney.bun@gmail.com>
2019-03-26 14:37:57 +0700

    change OpenMapSurfer_Roads basemap to terrain
    fix kartoza/geosafe#531

--- ingc_geonode_theme/settings.py
+++ ingc_geonode_theme/settings.py
@@ -126,3 +126,34 @@ LANGUAGES = (
     ('pt', 'Portuguese'),
     ('en', 'English'),
 )
+
+LEAFLET_CONFIG['TILES'] = {
+    ('OpenStreetMap',
+         'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> '
+         'contributors'),
+    ('Watercolor',
+         'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg',
+         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> \
+         &mdash; Map data &copy; \
+         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+         <a href="http://creativecommons.org/licenses/by-sa/2.0/"> \
+         CC-BY-SA</a>'),
+    ('Terrain',
+         'http://a.tile.stamen.com/terrain/{z}/{x}/{y}.png',
+         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> \
+         &mdash; Map data &copy; \
+         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+         <a href="http://creativecommons.org/licenses/by-sa/2.0/"> \
+         CC-BY-SA</a>'),
+    ('Toner Lite',
+         'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
+         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> \
+         &mdash; Map data &copy; \
+         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+         <a href="http://creativecommons.org/licenses/by-sa/2.0/"> \
+         CC-BY-SA</a>'),
+}
